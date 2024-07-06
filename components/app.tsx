87bc0ca3b67d192c5669c849f24184ebc44184cd
Léonard Jouve <leonard.jouve@gmail.com>
2024-07-06 12:06:38 +0200

    init

---
+++ components/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const App: React.FunctionComponent = () => (
+    <View>
+        <Text>Open up App.js to start working on your app!</Text>
+    </View>
+);
+
+export default App;
